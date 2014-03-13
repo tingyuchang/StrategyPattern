5a68af8b14a9ea9b28f7b43218cd1e308a144be4
tingyuchang <tingyugenius@gmail.com>
2014-03-13 19:07:52 +0800

    Initial Commit

---
+++ StrategyPattern/TYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TYAppDelegate.h
+//  StrategyPattern
+//
+//  Created by Ting-Yu Chang on 2014/3/13.
+//  Copyright (c) 2014年 Ting-Yu Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StrategyPattern/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StrategyPattern
+//
+//  Created by Ting-Yu Chang on 2014/3/13.
+//  Copyright (c) 2014年 Ting-Yu Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TYAppDelegate class]));
+    }
+}
